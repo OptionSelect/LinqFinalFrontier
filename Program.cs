adfcbf961b922a677cf5309bf135d150b2de73ae
Brandyn Sullins <woofenwauzer@gmail.com>
2017-08-22 13:31:49 -0400

    Spaceship converted from Java to C#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqFinalFrontier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
